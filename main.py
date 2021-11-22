84f77914a9b6a90cc42cada2d3489cedd4754c6d
hafidhrendyanto <hafidh.rendyanto@gmail.com>
2021-11-22 16:51:30 +0700

    add example notebook

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
 GPT-2 ASPECT BASED SENTIMENT ANALYSIS
 This is a demo application of an aspect based sentiment analyisis solution that uses GPT-2 as it's language processing model.
 You will enter a review text, and the system will output it's predicted aspect and sentiment pair.
-The GPT-2 model is fine tuned using SemEval 2016 ABSA Task 5 Restaurant & Laptop dataset, as such this aplication 
+The GPT-2 model is fine tuned using SemEval 2016 Task 5 Restaurant & Laptop dataset, as such this aplication 
 can only be tested on either restaurant or laptop dataset. 
 Please pick the domain of your data from the option below:
 1. Restaurant
